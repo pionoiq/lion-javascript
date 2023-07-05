490dff109ac6dd724f53ea46380d3a5a15b7775c
rerro <101159509+rorre0@users.noreply.github.com>
2023-07-05 16:24:32 +0900

    2일차 수업

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # 멋쟁이 사자처럼
 
----
-
 ## 자바스크립트 학습 저장소
-
----
 
 학습이 완료된 항목은 체크박스로 표시됩니다.
 
 ### Basic
 
-- [ ] Code Structure [바로가기](https://www.naver.com)
-- [ ] Variable [바로가기](https://www.naver.com)
-- [ ] Strict [바로가기](https://www.naver.com)
-- [ ] Global This [바로가기](https://www.naver.com)
-- [ ] Data Types [바로가기](https://www.naver.com)
-- [ ] Type Coversion [바로가기](https://www.naver.com)
+- [v] Code Structure [바로가기](https://www.naver.com)
+- [v] Variable [바로가기](https://www.naver.com)
+- [v] Strict [바로가기](https://www.naver.com)
+- [v] Global This [바로가기](https://www.naver.com)
+- [v] Data Types [바로가기](https://www.naver.com)
+- [v] Type Coversion [바로가기](https://www.naver.com)
+
+### Operations
+
+- [v] Operations (기본 연산자) [바로가기](https://www.naver.com)
+- [v] Operations (비교 연산자) [바로가기](https://www.naver.com)
---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,59 @@
+/* ------------------ */
+/* Variables          */
+
+/* 
+let name = 'John';
+let admin = name;
+console.log(admin);
+ */
+/* 
+name = 'tiger';
+admin = name;
+name = 'beom';
+console.log(admin);
+ */
+/* 
+const ourPlanetName = 'Earth';
+const currentOnlineUserName = 'user';
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+ */
+
+/* ------------------ */
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+// - 구매 제품 가격의 총 합
+// - 1년 기준 일(day)자 수
+// - 구매 결제 여부
+// - 구매 결제 내역
+// - 브랜드 접두사
+// - 오늘의 운세
+// - 상품 정보
+
+/* variables ----------------------------------------------------------- */
+
+// 범쌤 - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+//  - 구매 제품 가격의 총 합
+let TotalProductPrice;
+// - 구매 결제 여부
+let isPayment = false;
+// is, has가 들어가는 변수명은 보통 boolen 타입
+// - 오늘의 운세
+let fortuneOfToday;
+// - 구매 결제 내역
+let PaymentDetails;
+
+/* constant variables -------------------------------------------------- */
+
+// - 구매 제품 가격의 총 합
+const sumboughtProducts = 221;
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+// - 상품 정보
+const productInformation = '';
