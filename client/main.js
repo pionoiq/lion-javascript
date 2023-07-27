de88c8b616c76ba87c4dab2d27b2f4aa42373b9e
rerro <101159509+rorre0@users.noreply.github.com>
2023-07-27 16:37:31 +0900

    17일 수업
    -카드 랜더링

--- client/main.js
+++ client/main.js
@@ -1,3 +1,18 @@
-console.log('hello, js');
+import { getNode, getStorage, setStorage } from './lib/index.js';
 
-const a = 10;
+const textField = getNode('#textField');
+
+function handleTextField() {
+  const value = this.value;
+
+  setStorage('text', value);
+}
+
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+  });
+}
+
+textField.addEventListener('input', handleTextField);
+window.addEventListener('DOMContentLoaded', init);
